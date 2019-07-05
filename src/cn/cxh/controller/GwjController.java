e38467e26fa024ad9143418d16e49780b8910ae5
pytaq <1093499975@qq.com>
2019-07-05 10:12:06 +0800

    郭文佳创建的控制器servlet

---
+++ src/cn/cxh/controller/GwjController.java
@@ -0,0 +1,12 @@
+package cn.cxh.controller;
+/*    
+    Created by IntelliJ IDEA.
+    Package:cn.cxh.controller
+    User:1093499975@qq.com
+    Date:2019/7/5 0005
+    Time:10:10
+*/
+
+public class GwjController extends BaseController{
+
+}
